be24d06defd1d18dad9f58d535695f0a7353951b
Jun Obama <george@yk.rim.or.jp>
2022-01-14 11:23:40 +0900

    mod

--- run-release-image.sh
+++ run-release-image.sh
@@ -6,6 +6,6 @@ function docker-run-myoraclelinux7docker() {
     docker pull georgesan/myoraclelinux7docker:latest
     ${WINPTY_CMD} docker run -i -t --rm \
         -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-        georgesan/myoraclelinux7docker:latest
+        docker.io/georgesan/myoraclelinux7docker:latest
 }
 docker-run-myoraclelinux7docker
